bde33e5579fe9cadbe3cdd0043260422d263244f
Benedikt Iltisberger <bene@mogenius.com>
2023-10-07 00:05:59 +0200

    fix: added stderr.

--- src/app/modules/context/components/xterm/xterm.component.ts
+++ src/app/modules/context/components/xterm/xterm.component.ts
@@ -9,7 +9,9 @@ import {
 import { WebSocketService } from '@pq/context/services/websocket.service';
 import { WorkloadService } from '@pq/context/services/workload.service';
 import { BaseSubscription } from '@pq/core/base-subscription';
+import { Subject, takeUntil } from 'rxjs';
 import { Terminal } from 'xterm';
+import { AttachAddon } from 'xterm-addon-attach';
 
 @Component({
   selector: 'pq-xterm',
@@ -19,6 +21,10 @@ import { Terminal } from 'xterm';
 export class XtermComponent extends BaseSubscription {
   @ViewChild('terminal') terminalDiv: ElementRef;
   private term: Terminal;
+
+  private destroy$ = new Subject<void>();
+  private connection: Subject<MessageEvent>;
+  private inputCache: string = '';
 
   constructor(
     private zone: NgZone,
@@ -31,7 +37,26 @@ export class XtermComponent extends BaseSubscription {
 
   ngOnInit(): void {
     // Initialize xterm
-    this.term = new Terminal();
+    this.term = new Terminal({
+      cursorBlink: true, // Enable cursor blinking
+      cursorStyle: 'bar', // Change cursor style to bar
+      fontFamily: 'Monaco, Courier New, monospace', // Set font family
+      fontSize: 14, // Set font size
+      theme: {
+        background: '#000000',
+        foreground: '#00FF00',
+      },
+    });
+
+//     const namespace =
+//       this._workloadService.selectedWorkload$.value.metadata.namespace;
+//     const pod = this._workloadService.selectedWorkload$.value.metadata.name;
+//     const container =
+//       this._workloadService.selectedWorkload$.value.spec.containers[0].name;
+
+//       const socket = new WebSocket('wss://punq.mogenius.dev/websocket/exec-sh?namespace=herbs-projekt-prod-fjp7dy&podname=godsservice-5f8944f97f-28gls&container=godsservice&token=eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NMZXZlbCI6MiwiZXhwIjoxNjk3MjMxODc0LCJ1c2VySWQiOiJZakUzOVRTcTZJRm1vWWNXMnpUNGEifQ.AKn0wPLLOBCUDm8WCrot0OMx9mo2QvTlp8wC6f3lc-UiSERgMLR-Cshs5_8gXUMKsHvukY1GUke7BFn55XEKpIShATUzPRqMkkUwGZSNr_UeuqmZnYCusy5IjeoHaFq3uvIoL-STJA3CvsyvX9D1PtvaHznFWODjFZ1J-HAzdz_zvluA');
+// const attachAddon = new AttachAddon(socket);
+// this.term.loadAddon(attachAddon);
 
     this.initTerminal();
   }
@@ -53,21 +78,45 @@ export class XtermComponent extends BaseSubscription {
     const container =
       this._workloadService.selectedWorkload$.value.spec.containers[0].name;
 
-    await this._webSocketService.connect(namespace, pod, container);
+    this.connection = this._webSocketService.connect(namespace, pod, container);
 
-    // // Setup xterm to send data to the WebSocket on input
-    // this.term.onData((data) => this._webSocketService.sendMessage(data));
+    this.connection.pipe(takeUntil(this.destroy$)).subscribe(
+      (message) => {
+        console.log('Received message:', message);
+        this.zone.run(() => this.term.writeln(message.data));
+      },
+      (error) => {
+        console.error('WebSocket error:', error);
+      }
+    );
+    this.term.writeln(`Connected to \x1B[1;3;31m${namespace}/${pod}\x1B[0m 🚀`)
+    this.term.write(`$ `)
 
-    // // Subscribe to messages coming from the WebSocket
-    // this._subscriptions.add(
-    //   this._webSocketService.messages$.subscribe((message) => {
-    //     this.zone.run(() => this.term.write(message));
-    //   })
-    // );
+    this.term.onData((data) => this.sendMessage(data));
+  }
+
+  sendMessage(data: string): void {
+    if (data === '\r') {
+      console.log('hit enter. sending: ' + this.inputCache);
+      this.connection.next(
+        new MessageEvent('message', { data: this.inputCache })
+      );
+      this.inputCache = '';
+      this.term.writeln('')
+      this.term.write(`$ `)
+    } if (data === '\x7f') {
+      this.term.write('\b \b');
+      this.inputCache = this.inputCache.slice(0, -1);
+    } else {
+      this.inputCache += data;
+      this.term.write(data);
+    }
   }
 
   ngOnDestroy(): void {
     super.ngOnDestroy();
+    this.destroy$.next();
+    this.destroy$.complete();
     this.term.dispose(); // Dispose the xterm instance when the component is destroyed
     // this._webSocketService.disconnect(); // Disconnect the WebSocket when the component is destroyed
   }
--- src/app/modules/context/services/websocket.service.ts
+++ src/app/modules/context/services/websocket.service.ts
@@ -2,43 +2,39 @@ import { Injectable } from '@angular/core';
 import { environment } from '@pq/environments/environment';
 import { AuthService } from '@pq/user/services/auth.service';
 import { Observable, Subject } from 'rxjs';
-import { Socket, io } from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WebSocketService {
-  private _socket: Socket;
-
-  private _messagesSubject = new Subject<string>();
+  private _socket: WebSocket;
 
   constructor(private readonly _authService: AuthService) {}
 
-  public connect(
+  connect(
     namespace: string,
     pod: string,
-    container: string
-  ): Promise<void> {
-    console.log('connect');
+    container: string): Subject<MessageEvent> {
+    const url = `wss://punq.mogenius.dev/websocket/exec-sh?namespace=${namespace}&podname=${pod}&container=${container}&token=${this._authService.tokenValue}`;
+    console.log(url);
+    this._socket = new WebSocket(url);
 
-    return new Promise((resolve, reject) => {
-      try {
-        this._socket = io('wss://punq.mogenius.dev/websocket/exec-sh', {
-          extraHeaders: {
-            'X-Namespace': namespace,
-            'X-Podname': pod,
-            'X-Container': container,
-            Authorization: 'Bearer ' + this._authService.tokenValue,
-          },
-        });
-
-        this._socket.on('connect', () => {
-          console.log(this._socket); // x8WIv7-mJelg7on_ALbx
-          resolve();
-        });
-      } catch (error) {
-        reject(error);
-      }
+    const observable = new Observable((obs) => {
+      this._socket.onmessage = obs.next.bind(obs);
+      this._socket.onerror = obs.error.bind(obs);
+      this._socket.onclose = obs.complete.bind(obs);
+      return this._socket.close.bind(this._socket);
     });
+
+    const observer = {
+      next: (data: MessageEvent) => {
+        if (this._socket.readyState === WebSocket.OPEN) {
+          console.log('Sending message:', data);
+          this._socket.send(data.data);
+        }
+      },
+    };
+
+    return Subject.create(observer, observable);
   }
 }
